973f681fc4a1a8433f1df057f0ebc9391a9bb590
wk645 <wk645@nyu.edu>
2017-11-13 18:47:59 -0500

    minor fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,11 @@ const Home = () => {
 		<div>
 			<h1 className="title">News Plan</h1>
 			<center><Message floating className="homeP" compact={true}>
-				Apple has a mobile News application that lets users subscribe to various news sources to create a personalized set of news. 
+				Similar to Apple's News app, News Plan is a web version which compiles and delivers the users' news preferences, all in this website. You can select from over 50 different sources!
 				<br />
-				For the desktop alike, instead of having to go to different news websites, I thought it would be useful if I had a personalized set of news outlets that I regularly read, 
+				It's a personalized set of news reads that is condensed to the top trending stories, available on the go. 
 				<br />
-				and to only read their top stories when I do not have a whole ton of time to read the news. 
+
 			</Message></center>
 		</div>
 	)
