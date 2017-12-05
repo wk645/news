306b66354741be2fd5d412ccf9df25ab1a59915a
wk645 <wk645@nyu.edu>
2017-12-05 10:55:43 -0500

    added save article buttons, testing

--- src/components/cnn/CnnArticle.js
+++ src/components/cnn/CnnArticle.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import { Grid, Segment } from 'semantic-ui-react';
+import { Grid, Segment, Button } from 'semantic-ui-react';
 
 const CnnArticle = (props) => {
 
 	const handleClick = () => {
 		window.open(`${props.news.url}`)
+	}
+
+	const handleSave = (event) => {
+		console.log("pass cb f(x) here")
 	}
 
 	return (
@@ -14,6 +18,7 @@ const CnnArticle = (props) => {
 			<p className='description'>{props.news.description}</p>
 			<center><img src={props.news.urlToImage} alt='' /></center>
 			<br />
+			<Button className="articleButton" onClick={handleSave}>Save</Button>
 			</Segment>
 		</Grid.Column>
 	)
